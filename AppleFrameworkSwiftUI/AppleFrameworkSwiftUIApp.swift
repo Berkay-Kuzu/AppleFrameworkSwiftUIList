a99e7f498daf2e2005770fcd3b1fc5a5e56231e6
Berkay-Kuzu <berkay@papcorns.com>
2025-02-24 09:45:23 +0300

    Initial Commit

---
+++ AppleFrameworkSwiftUI/AppleFrameworkSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleFrameworkSwiftUIApp.swift
+//  AppleFrameworkSwiftUI
+//
+//  Created by Berkay on 24.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct AppleFrameworkSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

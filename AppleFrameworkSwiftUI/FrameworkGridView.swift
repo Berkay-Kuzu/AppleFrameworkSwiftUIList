bd9324406ba6ce52f90d73e20abfb1e20342e96c
Berkay-Kuzu <berkay@papcorns.com>
2025-02-24 11:36:02 +0300

    FrameworkGridView is created

---
+++ AppleFrameworkSwiftUI/FrameworkGridView.swift
@@ -0,0 +1,51 @@
+//
+//  FrameworkGridView.swift
+//  AppleFrameworkSwiftUI
+//
+//  Created by Berkay on 24.02.2025.
+//
+
+import SwiftUI
+
+struct FrameworkGridView: View {
+    
+    let columns: [GridItem] = [GridItem(.flexible()),
+                               GridItem(.flexible()),
+                               GridItem(.flexible())]
+    
+    var body: some View {
+        NavigationView {
+            ScrollView(showsIndicators: false) {
+                LazyVGrid(columns: columns) {
+                    ForEach(MockData.frameworks) { framework in
+                        FrameworkTitleView(framework: framework)
+                    }
+                }
+            }
+            .navigationTitle("🍎 Frameworks")
+        }
+    }
+}
+
+#Preview {
+    FrameworkGridView()
+        .preferredColorScheme(.dark)
+}
+
+struct FrameworkTitleView: View {
+    
+    let framework: Framework
+    
+    var body: some View {
+        VStack {
+            Image(framework.imageName)
+                .resizable()
+                .frame(width: 90, height: 90)
+            Text(framework.name)
+                .font(.title2)
+                .fontWeight(.semibold)
+                .scaledToFit()
+                .minimumScaleFactor(0.6)
+        }.padding(.all)
+    }
+}
